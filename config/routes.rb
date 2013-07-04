7c37019915493fdda11977dfb22deea416226567
Higorhicaro <higao_hh@hotmail.com>
2013-07-04 20:25:26 -0300

    Fiz o rank no players. Ideia do padeiro ainda de pé.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rank::Application.routes.draw do
+  get "home/index"
+
   resources :matches
 
 
@@ -54,7 +56,7 @@ Rank::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
--- db/migrate/20130703221933_create_matches.rb
+++ db/migrate/20130703221933_create_matches.rb
@@ -1,9 +1,9 @@
 class CreateMatches < ActiveRecord::Migration
   def change
     create_table :matches do |t|
-      t.references :players
-      t.references :players
-      t.references :players
+      t.integer :p1
+      t.integer :p2
+      t.integer :winner
       t.integer :pontuacao
 
       t.timestamps
